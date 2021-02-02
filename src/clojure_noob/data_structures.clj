1d135bd454dd7d2db6dabac263921c87de5637a4
Pragya Shrestha <pragyasresta@gmail.com>
2021-02-02 11:28:13 +0545

    Add numbers
    Signed-off-by: Pragya Shrestha <pragyasresta@gmail.com>

--- src/clojure_noob/data_structures.clj
+++ src/clojure_noob/data_structures.clj
@@ -1,1 +1,15 @@
 (ns clojure-noob.data-structures)
+
+; Clojure data-structures are immutable- similar to final list in java
+
+; Numbers
+
+;integer
+(println "integer: 12")
+
+;float
+(println "float: 1.2")
+
+;ratio
+(println "ratio: 1/5")
+
