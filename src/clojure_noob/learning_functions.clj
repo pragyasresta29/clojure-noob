9374430067a0e5ecf46f8875c5158831fbb93402
Pragya Shrestha <pragyasresta@gmail.com>
2021-02-02 15:05:09 +0545

    Learning about functions
    Signed-off-by: Pragya Shrestha <pragyasresta@gmail.com>

--- src/clojure_noob/learning_functions.clj
+++ src/clojure_noob/learning_functions.clj
@@ -80,3 +80,81 @@
 (println "Using default value - " (x-chop "jackie"))
 (println "Without default value - " (x-chop "jackie" "chop"))
 
+; Rest parameter "&"
+; Clojure also allows you to define variable-arity functions by
+; including a rest parameter, as in “put the rest of these arguments in a list
+; with the following name.” The rest parameter is indicated by an ampersand (&),
+; example
+(defn favorite-things
+  [name & things]
+  (str "Hi, " name ", here are my favorite things: "
+       (clojure.string/join ", " things)))
+
+(println "Rest \"&\" example - " (favorite-things "Doreen" "gum" "shoes" "kara-te"))
+
+; Destructuring
+; The basic idea behind destructuring is that it lets you
+; concisely bind names to values within a collection
+; example
+;; Return the first element of a collection
+(defn my-first
+  [[first-thing]]                                           ; Notice that first-thing is within a vector
+  first-thing)
+
+(println (my-first ["oven" "bike" "war-axe"]))
+; => "oven"
+
+; When destructuring a vector or list, you can name as many elements as you want
+; and also use rest parameters
+(defn chooser
+  [[first-choice second-choice & unimportant-choices]]
+  (println (str "Your first choice is: " first-choice))
+  (println (str "Your second choice is: " second-choice))
+  (println (str "We're ignoring the rest of your choices. "
+                "Here they are in case you need to cry over them: "
+                (clojure.string/join ", " unimportant-choices))))
+
+(println (chooser ["Marmalade", "Handsome Jack", "Pigpen", "Aquaman"]))
+
+; destructuring maps
+(defn location
+  [{address :address city :city}]
+  (println "Address: " address)
+  (println "City: " city))
+
+(println (location {:address "123 Street" :city "Kathmandu"}))
+
+; shortcut of destructuring maps by binding key and to variables
+(defn location
+  [{:keys [address city]}]
+  (println "Address: " address)
+  (println "City: " city))
+
+(println (location {:city "Kathmandu" :address "123 Street"}))
+
+; Function Body
+; The function body can contain forms of any kind.
+; Clojure automatically returns the last form evaluated.
+; This function body contains just three forms, and when you call the function,
+; it spits out the last form, "joe"
+
+(defn illustrative-function
+  []
+  (+ 1 304)
+  30
+  "joe")
+
+(illustrative-function)
+; => "joe"
+
+(defn my-if-function-body
+  "Returning using if expression"
+  [x]
+  (if (> x 6)
+    "Oh my gosh! What a big number!"
+    "That number's OK, I guess")
+  )
+
+(println (my-if-function-body 100))
+(println (my-if-function-body 1))
+
